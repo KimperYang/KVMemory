b9cc730535082425745d648bfe26c757a5a8871a
kimperyang <jingbo@ucsb.edu>
2024-08-07 09:11:21 +0000

    fixed sequence bug

--- inference.py
+++ inference.py
@@ -1,12 +1,13 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import DynamicCache
 import json
 
 def generate_kv(prompt):
 
     tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
     model = AutoModelForCausalLM.from_pretrained("meta-llama/Llama-2-7b-chat-hf", torch_dtype=torch.float16, device_map="auto")
-    inputs = tokenizer(prompt, return_tensors="pt", add_special_tokens=True).to(model.device)
+    inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
 
     out = model(**inputs, use_cache=True)
     past_key_values = out.past_key_values
@@ -44,7 +45,7 @@ def append_kv(kv_list):
 
     return concatenated_past_key_values
 
-def inference_with_kv(prompt, past_key_values, model_name="meta-llama/Llama-2-7b-chat-hf", max_length=50, num_return_sequences=1):
+def inference_with_kv(prompt, past_key_values, model_name="meta-llama/Llama-2-7b-chat-hf", max_length=200, num_return_sequences=1):
 
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype=torch.float16, device_map="auto")
@@ -74,11 +75,39 @@ def inference_with_kv(prompt, past_key_values, model_name="meta-llama/Llama-2-7b
     
     return generated_sequences
 
-start_kv = generate_kv("<s>")
-dummy_kv1 = generate_kv("hello") 
-dummy_kv2 = generate_kv("how are you")
+memory_list = ["hello\n", "how are you\n", "and how old are you\n"]
+template = "[INST] <<SYS>>\nYou are a helpful, respectful and honest assistant. Always answer as helpfully as possible.\n<</SYS>>\n"
+start_token = "<s>"
+end_token = "[/INST]"
+memory_list.insert(0, template)
+memory_list.insert(0, start_token)
+memory_list.append(end_token)
 
-appended_kv = append_kv([start_kv, dummy_kv1, dummy_kv2])
+kv_list = []
+seq = ""
+
+# for i in range(len(memory_list)):
+#     memory_list[i] = memory_list[i]
+
+for st in memory_list:
+    kv_list.append(generate_kv(st))
+    appended_kv = append_kv(kv_list)
+    seq = seq + st
+
 print(appended_kv[0][0].size())
-print(inference_with_kv("how old are you?", appended_kv))
+print(seq)
 
+print(inference_with_kv(seq, appended_kv))
+
+
+
+# start_kv = generate_kv("<s>")
+# template_kv = generate_kv("[INST] <<SYS>>\nYou are a helpful, respectful and honest assistant. Always answer as helpfully as possible.\n<</SYS>>\n\n [/INST]")
+# dummy_kv1 = generate_kv("hello") 
+# dummy_kv2 = generate_kv("how are you")
+# dummy_kv3 = generate_kv("and how old are you")
+
+
+# appended_kv = append_kv([template_kv, start_kv, dummy_kv1, dummy_kv2, dummy_kv3])
+# print(appended_kv[0][0].size())
+# print(inference_with_kv("[INST] <<SYS>>\nYou are a helpful, respectful and honest assistant. Always answer as helpfully as possible.\n<</SYS>>\n\n [/INST] hello ha ha ha how are you and how old are you", appended_kv))
