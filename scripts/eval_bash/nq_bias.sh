a0bba65a9effc030a17d4f4a5e9cbb71c119ff8f
Shiyu Chang Shiyu.Chang1@ibm.com <shiyuucsb@cccxl007.pok.ibm.com>
2025-01-16 17:25:29 -0500

    decay eval

--- scripts/eval_bash/nq_bias.sh
+++ scripts/eval_bash/nq_bias.sh
@@ -4,13 +4,13 @@ source /u/shiyuucsb/.bashrc
 conda activate /dccstor/scllm/envs/torchtune/
 cd /dccstor/scllm/KVMemory
 
-CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/nq/nq_torchtune.py --run "deter" --pos 0   &
-CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/nq/nq_torchtune.py --run "deter" --pos 1   &
-CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_torchtune.py --run "deter" --pos 2   &
-CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/nq/nq_torchtune.py --run "deter" --pos 3   &
-CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/nq/nq_torchtune.py --run "deter" --pos 4   &
-CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/nq/nq_torchtune.py --run "deter" --pos 5   &
-CUDA_VISIBLE_DEVICES=6 python scripts/evaluation/nq/nq_torchtune.py --run "deter" --pos 6   &
-CUDA_VISIBLE_DEVICES=7 python scripts/evaluation/nq/nq_torchtune.py --run "deter" --pos 7   &
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/nq/nq_torchtune.py --run "decay" --pos 0   &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/nq/nq_torchtune.py --run "decay" --pos 1   &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_torchtune.py --run "decay" --pos 2   &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/nq/nq_torchtune.py --run "decay" --pos 3   &
+CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/nq/nq_torchtune.py --run "decay" --pos 4   &
+CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/nq/nq_torchtune.py --run "decay" --pos 5   &
+CUDA_VISIBLE_DEVICES=6 python scripts/evaluation/nq/nq_torchtune.py --run "decay" --pos 6   &
+CUDA_VISIBLE_DEVICES=7 python scripts/evaluation/nq/nq_torchtune.py --run "decay" --pos 7   &
 
 wait
