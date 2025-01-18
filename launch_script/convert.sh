60277b20b6920b691e4334c95e644131f55478b0
hbr690188270 <bairu@ucsb.edu>
2025-01-18 04:35:28 +0000

    sync

--- launch_script/convert.sh
+++
@@ -1,2 +0,0 @@
-python -m torch.distributed.checkpoint.format_utils dcp_to_torch \
-    run_logs/decay model_cache/checkpoint.pt
--- launch_script/run.sh
+++
@@ -1,7 +0,0 @@
-LOG_RANK=${LOG_RANK:-0}
-NGPU=${NGPU:-"8"}
-
-PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True" \
-torchrun --nproc_per_node=8 --rdzv_backend c10d --rdzv_endpoint="localhost:0" \
-    --local-ranks-filter ${LOG_RANK} --role rank --tee 3 \
-    titan_trainer.py --config_name block_datav3_step10k_bsz64_single_node_selective_ckpt
