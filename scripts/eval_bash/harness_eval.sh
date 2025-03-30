23dc52ab924157017d33ea5e267ee2260ecb5a6d
kimperyang <jingbo@ucsb.edu>
2025-03-30 23:57:40 +0000

    update

--- scripts/eval_bash/harness_eval.sh
+++ scripts/eval_bash/harness_eval.sh
@@ -1,10 +1,8 @@
 # conda activate unlearning
 HF_ALLOW_CODE_EVAL="1"
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 accelerate launch -m lm_eval --model hf \
-    --num_processes 8 \
-    --num_machines 1 \
-    --model_args pretrained=training_res/new_data/upper_8B/checkpoint-6000,dtype="float" \
+CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes 4 --num_machines 1 -m lm_eval --model hf \
+    --model_args pretrained=/mnt/data2/jingbo/sum_5_8B/checkpoint-6000,dtype="float" \
     --tasks  arc_easy,arc_challenge,hellaswag,winogrande,piqa,sciq \
     --batch_size 16
 
