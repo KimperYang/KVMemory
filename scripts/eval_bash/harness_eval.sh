4916249e5330e5898ace6857ff8b78f6f25d4a5f
KimperYang <jingbo@ucsb.edu>
2025-05-06 19:40:52 +0000

    update

--- scripts/eval_bash/harness_eval.sh
+++ scripts/eval_bash/harness_eval.sh
@@ -1,9 +1,9 @@
 # conda activate unlearning
 HF_ALLOW_CODE_EVAL="1"
 
-CUDA_VISIBLE_DEVICES=3 accelerate launch --num_processes 1 --num_machines 1 -m lm_eval --model hf \
+CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes 4 --num_machines 1 -m lm_eval --model hf \
     --model_args pretrained=meta-llama/Llama-3.1-8B-Instruct,dtype="float" \
-    --tasks mmlu\
+    --tasks arc_easy,arc_challenge,hellaswag,winogrande,piqa,sciq \
     --apply_chat_template \
     --fewshot_as_multiturn \
     --batch_size 8
