0e5e00b7a6eb5bdc850d0eefe001cf1fa60f35ff
KimperYang <jingbo@ucsb.edu>
2025-05-11 17:26:01 +0000

    update

--- run2.sh
+++ run2.sh
@@ -6,5 +6,6 @@ run_name=$2
 export WANDB_API_KEY="297fefc6714432e38b47736829a56f96e540206a"
 
 python scripts/evaluation/musique/musique_sum.py --ckpt 1122 --run "training_res/sum/sum_5_31_8B_qa" --reencode 5
-python scripts/evaluation/musique/musique_block.py --ckpt 1122 --run "training_res/new_data/block_31_8B_qa" 
-python scripts/evaluation/tqa/tqa_sum.py --ckpt 1122 --run "training_res/sum/sum_5_31_8B_qa" --reencode 5
+python scripts/evaluation/2wiki/wiki_sum.py --ckpt 1122 --run "training_res/sum/sum_5_31_8B_qa" --reencode 5
+# python scripts/evaluation/musique/musique_block.py --ckpt 1122 --run "training_res/new_data/block_31_8B_qa" 
+# python scripts/evaluation/tqa/tqa_sum.py --ckpt 1122 --run "training_res/sum/sum_5_31_8B_qa" --reencode 5
