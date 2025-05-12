1bdc3663a1a016c0d2a7397c2ee4b8d38fbf2a25
KimperYang <jingbo@ucsb.edu>
2025-05-12 18:11:37 +0000

    update

--- run1.sh
+++ run1.sh
@@ -19,12 +19,12 @@ CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run 
 
 wait
 
-CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum/sum_0_31_8B" --reencode 0 --pos 4 &
-CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum/sum_0_31_8B" --reencode 0 --pos 5 &
-CUDA_VISIBLE_DEVICES=6 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum/sum_0_31_8B" --reencode 0 --pos 6 &
-CUDA_VISIBLE_DEVICES=7 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum/sum_0_31_8B" --reencode 0 --pos 7 &
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum/sum_0_31_8B" --reencode 0 --pos 4 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum/sum_0_31_8B" --reencode 0 --pos 5 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum/sum_0_31_8B" --reencode 0 --pos 6 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum/sum_0_31_8B" --reencode 0 --pos 7 &
 
 wait
 
-CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum/sum_0_31_8B" --reencode 0 --pos 8 &
-CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum/sum_0_31_8B" --reencode 0 --pos 9 &
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum/sum_0_31_8B" --reencode 0 --pos 8 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum/sum_0_31_8B" --reencode 0 --pos 9 &
--- run3.sh
+++ run3.sh
@@ -5,4 +5,14 @@ run_name=$2
 
 export WANDB_API_KEY="297fefc6714432e38b47736829a56f96e540206a"
 
-python scripts/evaluation/tqa/tqa_block.py --ckpt 1122 --run "training_res/new_data/block_31_8B_qa"
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/nq/nq_blend.py --pos 0 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/nq/nq_blend.py --pos 1 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_blend.py --pos 2 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/nq/nq_blend.py --pos 3 &
+
+wait
+
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/nq/nq_blend.py --pos 4 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/nq/nq_blend.py --pos 5 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_blend.py --pos 6 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/nq/nq_blend.py --pos 7 &
