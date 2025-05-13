fad781c0c778405b6e29955d31575ecb22973013
KimperYang <jingbo@ucsb.edu>
2025-05-13 05:17:04 +0000

    update

--- train2.sh
+++ train2.sh
@@ -1,5 +1,5 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --config_file config/4gpu_step4.yaml --main_process_port 25678 upper_qa_trainer.py --weight 8
-CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --config_file config/4gpu_step4.yaml --main_process_port 25678 upper_qa_trainer.py --weight 3
-CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --config_file config/4gpu_step4.yaml --main_process_port 25678 upper_qa_trainer.py --weight 1
-CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --config_file config/4gpu_step4.yaml --main_process_port 25678 blk_qa_trainer.py --weight 3
-CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --config_file config/4gpu_step4.yaml --main_process_port 25678 sum_qa_trainer.py --reencode 0 --weight 8
+CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --config_file config/4gpu_step4.yaml --main_process_port 25679 upper_qa_trainer.py --weight 8
+CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --config_file config/4gpu_step4.yaml --main_process_port 25679 upper_qa_trainer.py --weight 3
+CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --config_file config/4gpu_step4.yaml --main_process_port 25679 upper_qa_trainer.py --weight 1
+CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --config_file config/4gpu_step4.yaml --main_process_port 25679 blk_qa_trainer.py --weight 3
+CUDA_VISIBLE_DEVICES=4,5,6,7 accelerate launch --config_file config/4gpu_step4.yaml --main_process_port 25679 sum_qa_trainer.py --reencode 0 --weight 8
