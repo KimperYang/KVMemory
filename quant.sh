2ad42c738b4db782a5143f771e395d3299d0d406
KimperYang <jingbo@ucsb.edu>
2025-05-13 22:55:54 +0000

    update

--- quant.sh
+++ quant.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/quant/tqa_sum.py --ckpt 1122 --run "training_res/sum_5_qa" --reencode 5 &
-CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/quant/wiki_sum.py --ckpt 1122 --run "training_res/sum_5_qa" --reencode 5 &
-CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/quant/musique_sum.py --ckpt 1122 --run "training_res/sum_5_qa" --reencode 5 &
-CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/quant/hqa_sum.py --ckpt 1122 --run "training_res/sum_5_qa" --reencode 5 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/quant/tqa_sum.py --ckpt 1122 --run "training_res/sum_5_1B_qa" --reencode 5 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/quant/wiki_sum.py --ckpt 1122 --run "training_res/sum_5_1B_qa" --reencode 5 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/quant/musique_sum.py --ckpt 1122 --run "training_res/sum_5_1B_qa" --reencode 5 &
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/quant/hqa_sum.py --ckpt 1122 --run "training_res/sum_5_1B_qa" --reencode 5 &
