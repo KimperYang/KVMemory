7a356378da99a809135491267c67ab13208618af
KimperYang <jingbo@ucsb.edu>
2025-05-13 22:53:08 +0000

    update

---
+++ quant.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/quant/tqa_sum.py --ckpt 1122 --run "training_res/qa/3B/sum_5_3B_qa" --reencode 5 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/quant/wiki_sum.py --ckpt 1122 --run "training_res/qa/3B/sum_5_3B_qa" --reencode 5 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/quant/musique_sum.py --ckpt 1122 --run "training_res/qa/3B/sum_5_3B_qa" --reencode 5 &
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/quant/hqa_sum.py --ckpt 1122 --run "training_res/qa/3B/sum_5_3B_qa" --reencode 5 &
--- run1.sh
+++ run1.sh
@@ -11,8 +11,8 @@ CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/2wiki/wiki_sum.py --ckpt 1122 -
 
 # wait
 
-# CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/musique/musique_sum.py --ckpt 1122 --run "training_res/qa/3B/sum_5_3B_qa" --reencode 5 &
-# CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/hqa/hqa_sum.py --ckpt 1122 --run "training_res/qa/3B/sum_5_3B_qa" --reencode 5 &
+CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/musique/musique_sum.py --ckpt 1122 --run "training_res/qa/3B/sum_5_3B_qa" --reencode 5 &
+CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/hqa/hqa_sum.py --ckpt 1122 --run "training_res/qa/3B/sum_5_3B_qa" --reencode 5 &
 
 # python scripts/evaluation/compress/lingua.py --run "training_res/qa/3B/sum_5_3B_qa" --reencode 5 --ckpt 1122 --pos 0
 # python scripts/evaluation/compress/lingua.py --run "training_res/qa/3B/sum_5_3B_qa" --reencode 5 --ckpt 1122 --pos 1
