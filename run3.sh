88fece58057d88c61f0895104e4c0516ea52e4b9
KimperYang <jingbo@ucsb.edu>
2025-05-14 06:02:32 +0000

    update

--- run3.sh
+++ run3.sh
@@ -1,24 +1,24 @@
-CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/nq/nq_block.py --ckpt 1122 --run "training_res/qa/3B/block_3B_qa" --pos 0 &
-CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/nq/nq_block.py --ckpt 1122 --run "training_res/qa/3B/block_3B_qa" --pos 1 &
+
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/1B/sum_1_1B_qa" --reencode 1 --pos 0 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/1B/sum_1_1B_qa" --reencode 1 --pos 1 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/1B/sum_1_1B_qa" --reencode 1 --pos 2 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/1B/sum_1_1B_qa" --reencode 1 --pos 3 &
 
 wait
 
-CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/nq/nq_block.py --ckpt 1122 --run "training_res/qa/3B/block_3B_qa" --pos 2 &
-CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/nq/nq_block.py --ckpt 1122 --run "training_res/qa/3B/block_3B_qa" --pos 3 &
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/1B/sum_1_1B_qa" --reencode 1 --pos 4 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/1B/sum_1_1B_qa" --reencode 1 --pos 5 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/1B/sum_1_1B_qa" --reencode 1 --pos 6 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/1B/sum_1_1B_qa" --reencode 1 --pos 7 &
 
 wait
 
-CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/nq/nq_block.py --ckpt 1122 --run "training_res/qa/3B/block_3B_qa" --pos 4 &
-CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/nq/nq_block.py --ckpt 1122 --run "training_res/qa/3B/block_3B_qa" --pos 5 &
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/1B/sum_1_1B_qa" --reencode 1 --pos 8 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/1B/sum_1_1B_qa" --reencode 1 --pos 9 &
 
 wait
 
-CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/nq/nq_block.py --ckpt 1122 --run "training_res/qa/3B/block_3B_qa" --pos 6 &
-CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/nq/nq_block.py --ckpt 1122 --run "training_res/qa/3B/block_3B_qa" --pos 7 &
-
-wait
-
-CUDA_VISIBLE_DEVICES=5 python scripts/evaluation/nq/nq_block.py --ckpt 1122 --run "training_res/qa/3B/block_3B_qa" --pos 8 &
-CUDA_VISIBLE_DEVICES=4 python scripts/evaluation/nq/nq_block.py --ckpt 1122 --run "training_res/qa/3B/block_3B_qa" --pos 9 &
-
-wait
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/tqa/tqa_sum.py --ckpt 1122 --run "training_res/qa/1B/sum_1_1B_qa" --reencode 1 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/2wiki/wiki_sum.py --ckpt 1122 --run "training_res/qa/1B/sum_1_1B_qa" --reencode 1 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/musique/musique_sum.py --ckpt 1122 --run "training_res/qa/1B/sum_1_1B_qa" --reencode 1 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/hqa/hqa_sum.py --ckpt 1122 --run "training_res/qa/1B/sum_1_1B_qa" --reencode 1 &
