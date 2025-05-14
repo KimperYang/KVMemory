2745d1185c77e80df02c5436f39b1da75de0c5eb
KimperYang <jingbo@ucsb.edu>
2025-05-14 06:21:30 +0000

    update

--- run5.sh
+++ run5.sh
@@ -1,19 +1,19 @@
 
-python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/8B/sum_1_8B_qa" --reencode 1 --pos 0
-python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/8B/sum_1_8B_qa" --reencode 1 --pos 1
-python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/8B/sum_1_8B_qa" --reencode 1 --pos 2
+python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/sum/sum_1_8B_qa" --reencode 1 --pos 0
+python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/sum/sum_1_8B_qa" --reencode 1 --pos 1
+python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/sum/sum_1_8B_qa" --reencode 1 --pos 2
 
-python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/8B/sum_1_8B_qa" --reencode 1 --pos 3
-python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/8B/sum_1_8B_qa" --reencode 1 --pos 4
-python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/8B/sum_1_8B_qa" --reencode 1 --pos 5
+python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/sum/sum_1_8B_qa" --reencode 1 --pos 3
+python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/sum/sum_1_8B_qa" --reencode 1 --pos 4
+python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/sum/sum_1_8B_qa" --reencode 1 --pos 5
 
-python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/8B/sum_1_8B_qa" --reencode 1 --pos 6
-python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/8B/sum_1_8B_qa" --reencode 1 --pos 7
-python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/8B/sum_1_8B_qa" --reencode 1 --pos 8
+python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/sum/sum_1_8B_qa" --reencode 1 --pos 6
+python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/sum/sum_1_8B_qa" --reencode 1 --pos 7
+python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/sum/sum_1_8B_qa" --reencode 1 --pos 8
 
-python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/qa/8B/sum_1_8B_qa" --reencode 1 --pos 9
+python scripts/evaluation/nq/nq_sum.py --ckpt 1122 --run "training_res/sum/sum_1_8B_qa" --reencode 1 --pos 9
 
-python scripts/evaluation/tqa/tqa_sum.py --ckpt 1122 --run "training_res/qa/8B/sum_1_8B_qa" --reencode 1
-python scripts/evaluation/2wiki/wiki_sum.py --ckpt 1122 --run "training_res/qa/8B/sum_1_8B_qa" --reencode 1
-python scripts/evaluation/musique/musique_sum.py --ckpt 1122 --run "training_res/qa/8B/sum_1_8B_qa" --reencode 1
-python scripts/evaluation/hqa/hqa_sum.py --ckpt 1122 --run "training_res/qa/8B/sum_1_8B_qa" --reencode 1
+python scripts/evaluation/tqa/tqa_sum.py --ckpt 1122 --run "training_res/sum/sum_1_8B_qa" --reencode 1
+python scripts/evaluation/2wiki/wiki_sum.py --ckpt 1122 --run "training_res/sum/sum_1_8B_qa" --reencode 1
+python scripts/evaluation/musique/musique_sum.py --ckpt 1122 --run "training_res/sum/sum_1_8B_qa" --reencode 1
+python scripts/evaluation/hqa/hqa_sum.py --ckpt 1122 --run "training_res/sum/sum_1_8B_qa" --reencode 1
