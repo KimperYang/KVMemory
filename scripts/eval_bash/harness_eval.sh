e591200d4a082c54c216ca7db78fd31df1744393
KimperYang <jingbo@ucsb.edu>
2025-05-15 19:10:27 +0000

    update

--- scripts/eval_bash/harness_eval.sh
+++ scripts/eval_bash/harness_eval.sh
@@ -3,8 +3,10 @@ HF_ALLOW_CODE_EVAL="1"
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes 4 --num_machines 1 -m lm_eval --model hf \
     --model_args pretrained=training_res/sum_5_31_8B/checkpoint-6000,dtype="float" \
-    --tasks mmlu,arc_easy,arc_challenge,hellaswag,winogrande,piqa,sciq \
-    --batch_size 8
+    --tasks gsm8k_cot_llama, ifeval \
+    --batch_size 8 \
+    --apply_chat_template \
+    --fewshot_as_multiturn \
 
     # --tasks  arc_easy,arc_challenge,hellaswag,winogrande,piqa,sciq \
     # --confirm_run_unsafe_code \
