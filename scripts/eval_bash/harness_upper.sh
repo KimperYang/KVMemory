07818992f09d47482a03d9d6ab0a97645698c855
KimperYang <jingbo@ucsb.edu>
2025-05-15 19:30:51 +0000

    update

--- scripts/eval_bash/harness_upper.sh
+++ scripts/eval_bash/harness_upper.sh
@@ -2,7 +2,7 @@
 HF_ALLOW_CODE_EVAL="1"
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes 4 --num_machines 1 -m lm_eval --model hf \
-    --model_args pretrained=meta-llama/Llama-3.1-8B-Instruct,dtype="float" \
+    --model_args pretrained=training_res/upper_31_8B/checkpoint-6000,dtype="float" \
     --tasks gsm8k_cot_llama,ifeval \
     --batch_size 8 \
     --apply_chat_template \
@@ -12,4 +12,4 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes 4 --num_machines 
     # --confirm_run_unsafe_code \
 
     
-# training_res/upper_31_8B/checkpoint-6000
+# meta-llama/Llama-3.1-8B-Instruct
