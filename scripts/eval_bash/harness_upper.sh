ef60c0dec9fd4b324494a345b63a70ad5c082f90
KimperYang <jingbo@ucsb.edu>
2025-05-15 18:36:42 +0000

    lingua

--- scripts/eval_bash/harness_upper.sh
+++ scripts/eval_bash/harness_upper.sh
@@ -2,7 +2,7 @@
 HF_ALLOW_CODE_EVAL="1"
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes 4 --num_machines 1 -m lm_eval --model hf \
-    --model_args pretrained=training_res/upper_31_8B/checkpoint-6000,dtype="float" \
+    --model_args pretrained=meta-llama/Llama-3.1-8B-Instruct,dtype="float" \
     --tasks mmlu,arc_easy,arc_challenge,hellaswag,winogrande,piqa,sciq \
     --batch_size 8
 
@@ -11,4 +11,4 @@ CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes 4 --num_machines 
     # --apply_chat_template \
     # --fewshot_as_multiturn \
     
-
+# training_res/upper_31_8B/checkpoint-6000
