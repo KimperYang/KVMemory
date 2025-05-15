a1d0ef7664bc9363f7c978181f08323a96d53517
KimperYang <jingbo@ucsb.edu>
2025-05-15 09:34:51 +0000

    update

--- run6.sh
+++ run6.sh
@@ -1,23 +1,21 @@
-CUDA_VISIBLE_DEVICES=2
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 0
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 1
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 2
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 3
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 4
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 5
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 6
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 7
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 8
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 9
 
-python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 0
-python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 1
-python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 2
-python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 3
-python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 4
-python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 5
-python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 6
-python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 7
-python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 8
-python scripts/evaluation/nq/nq_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 --pos 9
-
-python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 0
-python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 1
-python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 2
-python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 3
-python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 4
-python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 5
-python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 6
-python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 7
-python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 8
-python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 9
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 0
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 1
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 2
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 3
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 4
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 5
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 6
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 7
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 8
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/nq/nq_block.py --ckpt 6000 --run "training_res/block_31_8B" --pos 9
--- scripts/eval_bash/harness_eval.sh
+++ scripts/eval_bash/harness_eval.sh
@@ -2,7 +2,7 @@
 HF_ALLOW_CODE_EVAL="1"
 
 CUDA_VISIBLE_DEVICES=0,1,2,3 accelerate launch --num_processes 4 --num_machines 1 -m lm_eval --model hf \
-    --model_args pretrained=meta-llama/Llama-3.1-8B-Instruct,dtype="float" \
+    --model_args pretrained=training_res/sum_5_31_8B/checkpoint-6000,dtype="float" \
     --tasks arc_easy,arc_challenge,hellaswag,winogrande,piqa,sciq \
     --apply_chat_template \
     --fewshot_as_multiturn \
