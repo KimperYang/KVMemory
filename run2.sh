10211a446b2cf347819bb2abe307c5a83369e1af
KimperYang <jingbo@ucsb.edu>
2025-05-15 05:15:15 +0000

    update

--- run2.sh
+++ run2.sh
@@ -1,11 +1,11 @@
-CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/samsum/samsum_sum.py --ckpt 6000 --run "training_res/sum_0_31_8B" --reencode 0 &
-CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/samsum/samsum_sum.py --ckpt 6000 --run "training_res/sum_1_31_8B" --reencode 1 &
-CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/samsum/samsum_sum.py --ckpt 6000 --run "training_res/sum_5_31_8B" --reencode 5 &
-CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/samsum/samsum_block.py --ckpt 6000 --run "block_31_8B" &
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/samsum/samsum_sum.py  --run "training_res/sum_0_31_8B" --reencode 0 &
+CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/samsum/samsum_sum.py  --run "training_res/sum_1_31_8B" --reencode 1 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/samsum/samsum_sum.py  --run "training_res/sum_5_31_8B" --reencode 5 &
+CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/samsum/samsum_block.py  --run "block_31_8B" &
 
 wait
 
-CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/samsum/samsum_upper.py --ckpt 6000 --run "meta-llama/Llama-3.1-8B-Instruct" &
+CUDA_VISIBLE_DEVICES=0 python scripts/evaluation/samsum/samsum_upper.py  --run "meta-llama/Llama-3.1-8B-Instruct" &
 CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/samsum/samsum_blend.py &
-CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/samsum/samsum_block.py --ckpt 6000 --run "meta-llama/Llama-3.1-8B-Instruct" &
-# CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/nq/nq_upper.py --ckpt 6000 --run "training_res/sum_5_31_8B" --pos 7 &
+CUDA_VISIBLE_DEVICES=2 python scripts/evaluation/samsum/samsum_block.py  --run "meta-llama/Llama-3.1-8B-Instruct" &
+# CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/nq/nq_upper.py  --run "training_res/sum_5_31_8B" --pos 7 &
