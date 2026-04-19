0b5a20da6106513a98049b367bbe7a267e660086
KimperYang <jingbo@ucsb.edu>
2026-04-19 08:00:35 +0000

    clear unnecessary scripts

--- run3.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=3 python scripts/evaluation/tqa/tqa_turbo.py  --run "/mnt/tmp/training_res/turbo/turbo_3B/checkpoint-6000" &
-# CUDA_VISIBLE_DEVICES=1 python scripts/evaluation/tqa/tqa_turbo.py  --run "training_res/sum_1_31_8B" &
-
-wait
